fc31374ddb008f3f6ddfec99d061f1f10fc7d8a2
Z4ck404 <zakaria.elbazi@netapp.com>
2023-12-07 00:10:18 +0100

    release module

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,14 +1,15 @@
 provider "aws" {
   region  = "us-west-2"
-  profile = "zack-labs"
+  profile = "bigdata-labs"
 }
 
 module "eks-s3-csi-driver" {
-  source  = "../.."
+  source  = "Z4ck404/eks-s3-csi-driver/aws"
+  version = "0.0.1"
 
-  aws_profile      = "zack-labs"
+  aws_profile      = "bigdata-labs"
   aws_region       = "us-west-2"
-  eks_cluster_name = "zack-demo-0"
+  eks_cluster_name = "demo-0"
 
   s3_bucket_name = "zack-s3-mount--usw2-az1--x-s3"
 }
