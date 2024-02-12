962dbea2bce416f848d8a828e43aa22cb60561c6
Fabio Rauber <fabiorauber@gmail.com>
2024-02-12 11:15:16 -0300

    Fix issue with S3 bucket access (#12)
    * Fix issue with S3 bucket access
    * Update version, fix S3 bucket IAM Policy

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_iam_policy" "s3_policy" {
           "s3:ListBucket"
         ],
         Resource = [
-          "arn:aws:s3:::${var.s3_bucket_name}/"
+          "arn:aws:s3:::${var.s3_bucket_name}"
         ],
       },
       {
